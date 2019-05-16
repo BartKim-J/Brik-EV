deda40c749cc6074c8088302f2f11f8e328238bb
bato <bart@xengineering.co>
2019-05-16 11:56:04 +0000

    backup script added

---
+++ lib/shell/brik_backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+VERSION_MAJOR=1
+VERSION_MINOR=0
+echo "Brik Back Script ( $VERSION_MAJOR.$VERSION_MINOR )"
+
+SRC=/
+DST=/mnt/Workspace/Brik/backup
+DATE="$(date +%m%d)"
+TIME="$(date +%H%M%S)"
+
+sudo fdisk -l
+read -p"dst disk : "  DST_DISK
+
+echo ${DST_DISK}
+
+echo "Mount USB Disk to /mnt directory."
+sudo mount ${DST_DISK} /mnt
+
+sudo mkdir -p ${DST}
+
+echo "Backup Start. $(date +%H%M%S)"
+sudo tar -cvpzf ${DST}/brik_${DATE}_${TIME}.tar.gz --exclude=/mnt --one-file-system ${SRC} 
+echo "Backup Done. $(date +%H%M%S)"
+
