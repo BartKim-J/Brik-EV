b57495182b29c808310b44f46f2b9a3cfe2fc1b3
bato <bart@xengineering.co>
2019-06-14 03:40:24 +0000

    brik_network.service setting up

--- lib/shell/run.sh
+++ lib/shell/run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-export SDL_NOMOUSE=1
-export SDL_FBDEV=/dev/fb0
-export SDL_VIDEODRIVER=fbcon
-/brik/brik_ev_c/bin/build/BrikEVC_exe
+# run #
+SDL_NOMOUSE=1 SDL_FBDEV=/dev/fb0 SDL_VIDEODRIVER=fbcon /brik/brik_ev_c/bin/build/BrikEVC_exe
